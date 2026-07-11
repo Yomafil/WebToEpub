9ff0eb55dbf7a132f6cc06c5dcd3ce315916d710
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-07-11 03:51:55 +0200

    Fix and Update Parser

--- plugin/js/parsers/CiweimaoParser.js
+++ plugin/js/parsers/CiweimaoParser.js
@@ -1,51 +1,60 @@
-parserFactory.register("www.ciweimao.com", () => new CiweimaoParser()); //wap.ciweimao.com has a different formating but has the same content as www.ciweimao.com
+parserFactory.register("www.ciweimao.com", () => new CiweimaoParser());
 
 class CiweimaoParser extends Parser {
     constructor() {
         super();
+        this.minimumThrottle = 1500;
+        this.imgChapterIds = new Set();
     }
 
+    //Make the first few page of the epub a tutorial on how to decrypt the epub after download?
+    // Or a link to a github page with a readme
+    // at least put the information somewhere in the epub, with an easy step by step
 
     async getChapterUrls(dom) {
-        // We need to call 'https://www.ciweimao.com/chapter/get_chapter_list_in_chapter_detail' to be sure we get the entire ToC
-        // We get the 'book_id' from the url 'www.ciweimao.com/book/book_id'
-        // POST : Request : Form data : book_id=book_id&chapter_id=0&orderby=0
-        // Response : JSON
-
-        let payload = new FormData();
+        let payload = new URLSearchParams();
         payload.append("book_id", this.getBookId(dom));
         payload.append("chapter_id", 0);
         payload.append("orderby", 0);
+
         let options = {
             method: "POST",
             credentials: "include",
             body: payload
         };
+
         let newDom = (await HttpClient.wrapFetch("https://www.ciweimao.com/chapter/get_chapter_list_in_chapter_detail", {fetchOptions: options})).responseXML;
 
-        // Because a book can be separated in volumes, we might get multiple ".book-chapter-list" each of them bound in a ".book-chapter-box" with the Volume Title stored in "h4 sub-tit"
-        // They also seems to restart the chapter count on new volume, but thats on per book basis and chapters are still properly ordered in the epub.
         let menuWrapper = document.createElement("div");
         let menu = [...newDom.querySelectorAll(".book-chapter-list")];
-        menu.forEach(element => menuWrapper.appendChild(element.cloneNode(true)));
-        // Not skilled enough to find a better solution then wrapping all of them in a parent element so hyperlinksToChapterList don't throw an error.
-        // Will probably get changed if we need to make our own implementation of hyperlinksToChapterList to handle VIP chapters, like QidianParser.
+        menu.forEach((element) => menuWrapper.appendChild(element.cloneNode(true)));
 
-        return util.hyperlinksToChapterList(menuWrapper);
+        this.imgChapterIds.clear();
+        let chapterLinks = [ ...menuWrapper.querySelectorAll("a[href*='/chapter']"),];
+
+        let chapters = chapterLinks.map((link) => {
+            return {
+                sourceUrl: link.href,
+                title: link.textContent.trim(),
+                isIncludeable: this.isLocked(link),
+            };
+        });
+
+        return chapters;
     }
 
-    /*
-    static isChapterVIP(chapter){
-        // We need to detect if a chapter is prefaced with a ".icon-lock", if it has one, then it's a VIP chapter.
-        // Example:  <li class=""> <a href="Chapter URL"> <i class="line"></i><i class='icon-lock'></i>Chapter Title</a></li>
-
-        // We could skip this step for non premium books if we need to, by checking if a book is VIP or FREE from the content tab of the book.
+    static isLocked(link) {
+        if (link.querySelector(".icon-lock")) {
+            return true; //locked
+        } 
+        else if (link.querySelector(".icon-unlock")) {
+            this.lockedChapterIds.add(this.getChapterId(link.href));
+            return false; //unlocked img
+        } 
+        else {
+            return false; //free
+        }
     }
-
-    static linksToChapter(){
-        // Gonna grab most of this from QidianParser, because they also need to deal with Premium Chapters.
-    }
-    */
 
     getBookId(dom) {
         return dom.baseURI.split("/").pop();
@@ -56,64 +65,144 @@ class CiweimaoParser extends Parser {
     }
     
     extractTitleImpl(dom) {
-        // Remove the span element from h1.title, as it includes the author's name.
         let title = dom.querySelector("h1.title");
-        let clone = title.cloneNode(true);
+        const clone = title.cloneNode(true);
         clone.querySelector("span")?.remove();
         return clone;
     }
 
     findContent(dom) {
-        return dom.querySelector("div"); 
-        // We can also have images in the encrypted chapter_content.
-        // The content is in "#J_BookRead"
+        return dom.querySelector("div");
     }
 
     async fetchChapter(url) {
-        // We need to call 'https://www.ciweimao.com/chapter/ajax_get_session_code' to get our 'chapter_access_key'
-        // We get the 'chapter_id' from the url 'www.ciweimao.com/chapter/chapter_id'
-        // POST : Request : Form data : chapter_id=chapter_id
-        // Response : JSON : {"code":100000,"chapter_access_key":"chapter_access_key"}
-
         let chapterId = this.getChapterId(url);
 
-        let payload = new FormData();
+        let rules = [
+            {
+                id: 1,
+                priority: 1,
+                action: {
+                    type: "modifyHeaders",
+                    requestHeaders: [
+                        {
+                            header: "referer",
+                            operation: "set",
+                            value: url,
+                        },
+                        {
+                            header: "origin",
+                            operation: "set",
+                            value: "https://www.ciweimao.com",
+                        },
+                    ],
+                },
+                condition: {
+                    urlFilter: "*://www.ciweimao.com/*",
+                },
+            },
+        ];
+
+        await HttpClient.setDeclarativeNetRequestRules(rules);
+
+        let payload = new URLSearchParams();
         payload.append("chapter_id", chapterId);
+
         let options = {
             method: "POST",
             credentials: "include",
             headers: {
                 "Accept": "application/json, text/javascript, */*; q=0.01",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "X-Requested-With": "XMLHttpRequest",
             },
-            body: payload
+            body: payload.toString()
         };
-        let json = (await HttpClient.fetchJson("https://www.ciweimao.com/chapter/ajax_get_session_code", options)).json;
 
-        // We then need to call 'https://www.ciweimao.com/chapter/get_book_chapter_detail_info' to get the 'chapter_content'
-        // POST : Request : Form data : chapter_id=chapter_id&chapter_access_key=chapter_access_key
-        // Response : JSON : {"code":100000,"rad":401,"encryt_keys":["",""],"chapter_content":"chapter_content"}
+        let chapterJson;
+        if (this.imgChapterIds.has(chapterId)) {
+            //unlock img
+            chapterJson = (await HttpClient.fetchJson("https://www.ciweimao.com/chapter/ajax_get_image_session_code", options)).json;
+        }
+        else {
+            //free
+            let chapterAjaxKeyJson = (await HttpClient.fetchJson("https://www.ciweimao.com/chapter/ajax_get_session_code", options)).json; 
+            if (chapterAjaxKeyJson.code != 100000) {
+                throw new Error(chapterAjaxKeyJson.code + " " + chapterAjaxKeyJson.chapter_access_key);
+            }
 
-        // cheating a bit, we're adding new data to payload and re-using the payload and options
-        payload.append("chapter_access_key", json.chapter_access_key);
-        json = (await HttpClient.fetchJson("https://www.ciweimao.com/chapter/get_book_chapter_detail_info", options)).json;
+            let chapterAccessKey = chapterAjaxKeyJson.chapter_access_key;
+            payload = new URLSearchParams();
+            payload.append("chapter_id", chapterId);
+            payload.append("chapter_access_key", chapterAjaxKeyJson.chapter_access_key);
 
-        // Huge skill issue here. Might be a naive approach. Not skilled enough to say.
-        let chapter_content = document.createElement("div");
-        chapter_content.textContent = json?.chapter_content; 
-        // 'chapter_content' appears to be undefined, or might be removed along the way, or some other stuff that I don't know about. ¯\_(ツ)_/¯
-        // I'm probably just grabbing it wrong.
-        return chapter_content;
+            options.body = payload.toString();
+
+            let chapterDetailJson = (await HttpClient.fetchJson("https://www.ciweimao.com/chapter/get_book_chapter_detail_info", options)).json;
+            if (chapterDetailJson.code != 100000) {
+                throw new Error(chapterDetailJson.code + " " + chapterDetailJson.tip);
+            }
+
+            chapterJson = { ...chapterDetailJson, chapterAccessKey};
+        }
+
+        return this.buildEncryptedChapter(chapterJson, url);
     }
 
-    
+    async buildEncryptedChapter(chapterJson, url) {
+        let newDoc = Parser.makeEmptyDocForContent(url);
+        let chapterId = this.getChapterId(url);
 
-    removeUnwantedElementsFromContentElement(element) {
-        util.removeChildElementsMatchingSelector(element, "span"); //We need to remove the span from every p.chapter.
-        super.removeUnwantedElementsFromContentElement(element);
-    }
+        //unlocked img
+        if (this.imgChapterIds.has(chapterId)) {
+            let heightUrl = new URL("https://www.ciweimao.com/chapter/get_book_chapter_image_height");
+            let options = {
+                chapter_id: chapterId,
+                area_width: 871,
+                font: "undefined",
+                font_size: 16,
+                bg_color_name: "white",
+                text_color_name: "white",
+            };
+            heightUrl.search = new URLSearchParams(options).toString();
+            await HttpClient.wrapFetch(heightUrl.href);
 
-    findChapterTitle(dom) {
-        return dom.querySelector(".read-hd h1");
+            let imageUrl = new URL("https://www.ciweimao.com/chapter/book_chapter_image");
+            imageUrl.search = new URLSearchParams({
+                ...options,
+                image_code: chapterJson.image_code,
+            }).toString();
+
+            let img = newDoc.dom.createElement("img");
+            img.src = imageUrl.href;
+            newDoc.content.appendChild(img);
+        }
+        //free
+        else {
+            let chapterText = chapterJson.chapter_content;
+            let encryptKeys = chapterJson.encryt_keys;
+            let chapterAccessKey = chapterJson.chapter_access_key;
+
+            let tmpDiv = newDoc.dom.createElement("div");
+            tmpDiv.innerHTML = chapterText;
+            while (tmpDiv.firstChild) {
+                newDoc.content.appendChild(tmpDiv.firstChild);
+            }
+
+            let encryptKeyDiv = newDoc.dom.createElement("div");
+            encryptKeyDiv.innerHTML = encryptKeys;
+            while (encryptKeyDiv.firstChild) {
+                newDoc.content.appendChild(encryptKeyDiv.firstChild);
+            }
+
+            let chapterAccessKeyDiv = newDoc.dom.createElement("div");
+            chapterAccessKeyDiv.innerHTML = chapterAccessKey;
+            while (chapterAccessKeyDiv.firstChild) {
+                newDoc.content.appendChild(chapterAccessKeyDiv.firstChild);
+            }
+        }
+
+        return newDoc.dom;
     }
 
     findCoverImageUrl(dom) {
@@ -121,7 +210,7 @@ class CiweimaoParser extends Parser {
     }
 
     extractLanguage() {
-        return "zh-CN"; //html lang is erroneously set to "en" on the website, but the site and books are in chinese.
+        return "zh-CN";
     }
 
     extractAuthor(dom) {
@@ -129,11 +218,25 @@ class CiweimaoParser extends Parser {
         return authorLabel?.textContent ?? super.extractAuthor(dom);
     }
 
+    extractPublisher() {
+        return "Ciweimao";
+    }
+
     getInformationEpubItemChildNodes(dom) {
         return [...dom.querySelectorAll(".book-bd")];
     }
 
     cleanInformationNode(node) {
-        return node; //We need to remove universal book-tip from the book description:（本站郑重提醒: 本故事纯属虚构，如有雷同，纯属巧合，切勿模仿。)
+        const elementsToRemove = node.querySelectorAll(
+            ".book-tip, [style*=\"display:none\"]"
+        );
+        elementsToRemove.forEach((el) => el.remove());
+        return node;
+    }
+
+    addTitleToContent(webPage, content) {
+        let h2 = webPage.rawDom.createElement("h2");
+        h2.innerText = webPage.title.trim();
+        content.prepend(h2);
     }
 }
